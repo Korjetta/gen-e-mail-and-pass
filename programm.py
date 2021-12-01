a2ccab33733639efd17bc74624863bfe10e89ca0
A.Kozhevnikova <alina.novikova@bk.ru>
2021-12-01 23:26:01 +0300

    mail address creation function

---
+++ programm.py
@@ -0,0 +1,10 @@
+# функция создания почтовых адресов
+
+def email_gen(list_of_names):
+    emails = []
+    for i in list_of_names:
+        letter = 1
+        while i[1] + '.' + i[0][0:letter] + '@company.io' in emails:
+            letter += 1
+        emails.append(i[1] + '.' + i[0][0:letter] + '@company.io')
+    return emails
